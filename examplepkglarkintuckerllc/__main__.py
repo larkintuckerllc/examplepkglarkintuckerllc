8e244160b3e2fbe90b5d316bed85ce017674b5bf
John Tucker <john@larkintuckerllc.com>
2020-10-05 09:51:21 -0400

    create import package files

---
+++ examplepkglarkintuckerllc/__main__.py
@@ -0,0 +1,6 @@
+"""Provide content for package execution."""
+from examplepkglarkintuckerllc import example_module
+
+
+_greeting = example_module.hello()
+print(_greeting)
---
+++ examplepkglarkintuckerllc/example_module.py
@@ -0,0 +1,6 @@
+"""Provide an example module."""
+
+
+def hello():
+    """Return greeting."""
+    return 'Hello World!'
